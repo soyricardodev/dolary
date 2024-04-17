a91f5f1049fd6d23f8d9651ae0c71071cde59bab
Ricardo Castro <soyricardodev@proton.me>
2024-04-17 12:43:02 -0400

    remove unused update function

---
+++ src/server/queries.ts
@@ -0,0 +1,35 @@
+import type { AllCurrencies } from "@/query/all";
+
+export const mockData: AllCurrencies = {
+	data: [
+		{
+			label: "Paralelo",
+			name: "BS/USD",
+			currency: "USD",
+			value: 20,
+			color: "rgb(34 197 94)",
+		},
+		{
+			label: "BCV",
+			name: "BS/USD",
+			currency: "USD",
+			value: 0,
+			color: "rgb(234 179 8)",
+		},
+		{
+			label: "Euro",
+			name: "BS/EUR",
+			currency: "EUR",
+			value: 20,
+			color: "rgb(59 130 246)",
+		},
+		{
+			label: "Yuan",
+			name: "BS/CNY",
+			currency: "CNY",
+			value: 0,
+			color: "rgb(225 29 72)",
+		},
+	],
+};
+

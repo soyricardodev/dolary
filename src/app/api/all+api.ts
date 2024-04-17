a3fed56235dc83f92809875c8d7b645a6dd33404
Ricardo Castro <soyricardodev@proton.me>
2024-04-17 12:42:12 -0400

    use redis from api

--- src/app/api/all+api.ts
+++ src/app/api/all+api.ts
@@ -1,39 +1,9 @@
-import { getBcvData, getParaleloData } from "@/lib/scrapper/get-data";
+import { kv } from '@vercel/kv';
 
 export async function GET() {
-	const { usd: usdParalelo } = await getParaleloData();
-	const { usd: usdBcv, eur, yuan } = await getBcvData();
+	const dataFromStorage = await kv.get("currencies")
 
 	return Response.json({
-		data: [
-			{
-				label: "Paralelo",
-				name: "BS/USD",
-				currency: "USD",
-				value: usdParalelo,
-				color: "rgb(34 197 94)"
-			},
-			{
-				label: "BCV",
-				name: "BS/USD",
-				currency: "USD",
-				value: usdBcv,
-				color: "rgb(234 179 8)"
-			},
-			{
-				label: "Euro",
-				name: "BS/EUR",
-				currency: "EUR",
-				value: eur,
-				color: "rgb(59 130 246)"
-			},
-			{
-				label: "Yuan",
-				name: "BS/CNY",
-				currency: "CNY",
-				value: yuan,
-				color: "rgb(225 29 72)"
-			}
-		],
+		dataFromStorage
 	});
 }

b9e799395aaf3a2468ad55b4acef3cf7ab0af6bc
Ricardo Castro <soyricardodev@proton.me>
2024-04-17 12:42:27 -0400

    add endpoint for cron updates

---
+++ src/app/api/cron+api.ts
@@ -0,0 +1,42 @@
+import { kv } from "@vercel/kv";
+import { getBcvData, getParaleloData } from "@/lib/scrapper/get-data";
+
+export async function GET() {
+	const { usd: usdParalelo } = await getParaleloData();
+	const { usd: usdBcv, eur, yuan } = await getBcvData();
+
+	const data = [
+		{
+			label: "Paralelo",
+			name: "BS/USD",
+			currency: "USD",
+			value: usdParalelo,
+			color: "rgb(34 197 94)",
+		},
+		{
+			label: "BCV",
+			name: "BS/USD",
+			currency: "USD",
+			value: usdBcv,
+			color: "rgb(234 179 8)",
+		},
+		{
+			label: "Euro",
+			name: "BS/EUR",
+			currency: "EUR",
+			value: eur,
+			color: "rgb(59 130 246)",
+		},
+		{
+			label: "Yuan",
+			name: "BS/CNY",
+			currency: "CNY",
+			value: yuan,
+			color: "rgb(225 29 72)",
+		},
+	];
+
+	await kv.set("currencies", JSON.stringify(data));
+
+	return Response.json({ status: 200, message: "Data updated" });
+}

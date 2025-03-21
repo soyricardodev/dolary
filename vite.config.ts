376a2ea85df3b217985fa26d8544802c17325c4c
Ricardo Castro <soyricardodev@proton.me>
2025-03-21 00:37:37 -0400

    add icons

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import { PWAConfig } from './src/lib/pwa-config'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA(PWAConfig)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

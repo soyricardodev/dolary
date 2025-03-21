712c58bba8f5213683a64612eebb3a0c587885bd
Ricardo Castro <soyricardodev@proton.me>
2025-03-21 00:39:36 -0400

    add bearer key

--- src/hooks/use-currency-data.tsx
+++ src/hooks/use-currency-data.tsx
@@ -52,7 +52,11 @@ export function useCurrencyData() {
     queryKey: ["dollarRates"],
     queryFn: async () => {
       try {
-        const response = await fetch("https://pydolarve.org/api/v1/dollar?rounded_price=false")
+        const response = await fetch("https://pydolarve.org/api/v1/dollar?rounded_price=false", {
+          headers: {
+            Authorization: `Bearer ${import.meta.env.PYDOLAR_KEY}`,
+          }
+        })
         if (!response.ok) {
           throw new Error("Network response was not ok")
         }

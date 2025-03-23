87f9b1ec1664a2544db2ee6982200f7775473af6
Ricardo Castro <soyricardodev@proton.me>
2025-03-22 21:42:06 -0400

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { AppHeader } from "./components/app-header";
 import { CurrencyMonitors } from "./components/currency-monitors";
 import { CurrencyCalculator } from "./components/currency-calculator";
 import { AppFooter } from "./components/app-footer";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
 	const [selectedCurrency, setSelectedCurrency] = useState<string | null>(null);
@@ -18,22 +19,25 @@ export default function App() {
 	};
 
 	return (
-		<main className="min-h-screen p-3 flex flex-col font-sans">
-			<AppHeader />
+		<>
+			<Analytics />
+			<main className="min-h-screen p-3 flex flex-col font-sans">
+				<AppHeader />
 
-			<div className="flex-grow flex flex-col justify-center">
-				<CurrencyMonitors onCardClick={handleCardClick} />
-			</div>
+				<div className="flex-grow flex flex-col justify-center">
+					<CurrencyMonitors onCardClick={handleCardClick} />
+				</div>
 
-			<AppFooter />
+				<AppFooter />
 
-			{selectedCurrency && (
-				<CurrencyCalculator
-					currency={selectedCurrency}
-					isOpen={calculatorVisible}
-					onClose={closeCalculator}
-				/>
-			)}
-		</main>
+				{selectedCurrency && (
+					<CurrencyCalculator
+						currency={selectedCurrency}
+						isOpen={calculatorVisible}
+						onClose={closeCalculator}
+					/>
+				)}
+			</main>
+		</>
 	);
 }

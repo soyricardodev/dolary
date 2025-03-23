51130a61aabfb8fcdf02d63c25b4f31bbce7c2f0
Ricardo Castro <soyricardodev@proton.me>
2025-03-22 21:31:00 -0400

    add drawer

---
+++ src/components/ui/drawer.tsx
@@ -0,0 +1,125 @@
+import { Drawer as DrawerPrimitive } from "vaul";
+
+import type * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+const Drawer = ({
+	shouldScaleBackground = true,
+	...props
+}: React.ComponentProps<typeof DrawerPrimitive.Root>) => (
+	<DrawerPrimitive.Root
+		shouldScaleBackground={shouldScaleBackground}
+		{...props}
+	/>
+);
+Drawer.displayName = "Drawer";
+
+const DrawerTrigger = DrawerPrimitive.Trigger;
+
+const DrawerPortal = DrawerPrimitive.Portal;
+
+const DrawerClose = DrawerPrimitive.Close;
+
+function DrawerOverlay({
+	className,
+	...props
+}: React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Overlay>) {
+	return (
+		<DrawerPrimitive.Overlay
+			className={cn("fixed inset-0 z-50 bg-overlay", className)}
+			{...props}
+		/>
+	);
+}
+DrawerOverlay.displayName = DrawerPrimitive.Overlay.displayName;
+
+function DrawerContent({
+	className,
+	children,
+	...props
+}: React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Content>) {
+	return (
+		<DrawerPortal>
+			<DrawerOverlay />
+			<DrawerPrimitive.Content
+				className={cn(
+					"fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-base border-2 border-border bg-bg font-heading",
+					className,
+				)}
+				{...props}
+			>
+				<div className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-blank" />
+				{children}
+			</DrawerPrimitive.Content>
+		</DrawerPortal>
+	);
+}
+DrawerContent.displayName = "DrawerContent";
+
+const DrawerHeader = ({
+	className,
+	...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+	<div
+		className={cn(
+			"grid gap-1.5 p-4 text-center font-heading sm:text-left",
+			className,
+		)}
+		{...props}
+	/>
+);
+DrawerHeader.displayName = "DrawerHeader";
+
+const DrawerFooter = ({
+	className,
+	...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+	<div
+		className={cn("mt-auto flex flex-col gap-2 p-4 font-heading", className)}
+		{...props}
+	/>
+);
+DrawerFooter.displayName = "DrawerFooter";
+
+function DrawerTitle({
+	className,
+	...props
+}: React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Title>) {
+	return (
+		<DrawerPrimitive.Title
+			className={cn(
+				"text-lg font-heading leading-none tracking-tight",
+				className,
+			)}
+			{...props}
+		/>
+	);
+}
+DrawerTitle.displayName = DrawerPrimitive.Title.displayName;
+
+function DrawerDescription({
+	className,
+	...props
+}: React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Description>) {
+	return (
+		<DrawerPrimitive.Description
+			className={cn("text-sm font-base text-text", className)}
+			{...props}
+		/>
+	);
+}
+DrawerDescription.displayName = DrawerPrimitive.Description.displayName;
+
+export {
+	Drawer,
+	DrawerPortal,
+	DrawerOverlay,
+	DrawerTrigger,
+	DrawerClose,
+	DrawerContent,
+	DrawerHeader,
+	DrawerFooter,
+	DrawerTitle,
+	DrawerDescription,
+};

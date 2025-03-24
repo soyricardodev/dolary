c0a46cfd60d5e0b67f0527d2f9977f3111c80064
Ricardo Castro <soyricardodev@proton.me>
2025-03-23 21:09:48 -0400

    fix github link and add correct icon

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,21 +1,23 @@
-import { Github } from "lucide-react"
+import { GithubIcon } from "./icons/github-icon";
 
 export function AppFooter() {
-  return (
-    <footer className="text-center text-xs text-gray-500">
-      <div className="flex items-center justify-center gap-1">
-        <p className="flex gap-1">Creado por{" "}<a
-          href="https://github.com/ricardocr987"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="GitHub de Ricardo Castro"
-          className="flex items-center gap-1 hover:underline"
-        >Ricardo Castro
-
-          <Github className="h-3.5 w-3.5" />
-        </a></p>
-      </div>
-    </footer>
-  )
+	return (
+		<footer className="text-center text-xs text-gray-500">
+			<div className="flex items-center justify-center gap-1">
+				<p className="flex gap-1">
+					Creado por{" "}
+					<a
+						href="https://github.com/soyricardodev"
+						target="_blank"
+						rel="noopener noreferrer"
+						aria-label="GitHub de Ricardo Castro"
+						className="flex items-center gap-1 hover:underline font-semibold"
+					>
+						Ricardo Castro
+						<GithubIcon className="size-4 text-black" fill="#000" />
+					</a>
+				</p>
+			</div>
+		</footer>
+	);
 }
-

746c7abe2b7b7269d81e906ba99bd25c0194c1db
Ricardo Castro <soyricardodev@proton.me>
2025-03-24 16:05:21 -0400

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,50 @@
 import { Providers } from "../components/providers";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./index.css";
 import { Analytics } from "@vercel/analytics/react";
 
+const APP_NAME = "Dolary";
+const APP_DEFAULT_TITLE = "Calculadora Dolar Venezuela";
+const APP_TITLE_TEMPLATE = "%s - Dolary";
+const APP_DESCRIPTION =
+	"La mejor calculadora sin anuncios, gratis, rápida y sin acceso a internet para Venezuela!";
+
 export const metadata: Metadata = {
-	title: "Calculadora Dolar Venezuela | Dolary",
-	description: "Calculadora Dolar Venezuela | Dolary",
+	applicationName: APP_NAME,
+	title: {
+		default: APP_DEFAULT_TITLE,
+		template: APP_TITLE_TEMPLATE,
+	},
+	description: APP_DESCRIPTION,
+	appleWebApp: {
+		capable: true,
+		statusBarStyle: "default",
+		title: APP_DEFAULT_TITLE,
+	},
+	formatDetection: {
+		telephone: false,
+	},
+	openGraph: {
+		type: "website",
+		siteName: APP_NAME,
+		title: {
+			default: APP_DEFAULT_TITLE,
+			template: APP_TITLE_TEMPLATE,
+		},
+		description: APP_DESCRIPTION,
+	},
+	twitter: {
+		card: "summary",
+		title: {
+			default: APP_DEFAULT_TITLE,
+			template: APP_TITLE_TEMPLATE,
+		},
+		description: APP_DESCRIPTION,
+	},
+};
+
+export const viewport: Viewport = {
+	themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({
@@ -13,9 +52,6 @@ export default function RootLayout({
 }: { children: React.ReactNode }) {
 	return (
 		<html lang="es">
-			<head>
-				<link rel="icon" type="image/svg+xml" href="/vite.svg" />
-			</head>
 			<body>
 				<Analytics />
 				<Providers>{children}</Providers>

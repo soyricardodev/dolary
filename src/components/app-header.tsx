4fbbb4dacf39718830282d5a92eee688fc6a211f
Ricardo Castro <soyricardodev@proton.me>
2025-03-24 19:10:43 -0400

    add better icon

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -1,18 +1,21 @@
 "use client";
 
-import { DollarSign, RefreshCw } from "lucide-react";
+import { RefreshCw } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useCurrencyData } from "@/hooks/use-currency-data";
+import Link from "next/link";
 
 export function AppHeader() {
 	const { refetch, isRefetching } = useCurrencyData();
 
 	return (
 		<header className="flex w-full justify-between items-center">
-			<Button>
-				<DollarSign className="size-5" />
-				<span className="text-xl font-bold">Dolary</span>
-			</Button>
+			<Link
+				href="/"
+				className="text-[30px] h-11 w-11 rounded-base flex bg-main text-text border-2 border-black items-center justify-center font-heading"
+			>
+				D
+			</Link>
 
 			<div className="flex justify-center">
 				<Button size="icon" onClick={() => refetch()} disabled={isRefetching}>

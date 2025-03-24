4adf664823a6336553925c87a25544086bf405cc
Ricardo Castro <soyricardodev@proton.me>
2025-03-24 07:33:13 -0400

    remove vite-vercel-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,10 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
 import { PWAConfig } from "./src/lib/pwa-config";
-import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
-		vercel(),
 		react(),
 		tailwindcss(),
 		VitePWA(PWAConfig),

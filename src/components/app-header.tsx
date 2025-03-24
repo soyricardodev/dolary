77ffb1ed8564269f6b7bf825b4b5ea9b7a77dd46
Ricardo Castro <soyricardodev@proton.me>
2025-03-23 21:10:25 -0400

    remove unused margin

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -6,13 +6,13 @@ export function AppHeader() {
 	const { refetch, isRefetching } = useCurrencyData();
 
 	return (
-		<header className="mb-3 text-center flex w-full justify-between items-center">
+		<header className="flex w-full justify-between items-center">
 			<Button>
 				<DollarSign className="size-5" />
 				<span className="text-xl font-bold">Dolary</span>
 			</Button>
 
-			<div className="flex justify-center mt-2">
+			<div className="flex justify-center">
 				<Button size="icon" onClick={() => refetch()} disabled={isRefetching}>
 					<RefreshCw
 						className={`h-3.5 w-3.5 mr-1 ${isRefetching ? "animate-spin" : ""}`}

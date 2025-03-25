432a31912a7ea3b1749a424381bd766554d05dcd
Ricardo Castro <soyricardodev@proton.me>
2025-03-25 13:30:11 -0400

    add notifications events

--- src/app/sw.ts
+++ src/app/sw.ts
@@ -7,19 +7,42 @@ import { Serwist } from "serwist";
 // actual precache manifest. By default, this string is set to
 // `"self.__SW_MANIFEST"`.
 declare global {
-  interface WorkerGlobalScope extends SerwistGlobalConfig {
-    __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
-  }
+	interface WorkerGlobalScope extends SerwistGlobalConfig {
+		__SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
+	}
 }
 
 declare const self: ServiceWorkerGlobalScope;
 
 const serwist = new Serwist({
-  precacheEntries: self.__SW_MANIFEST,
-  skipWaiting: true,
-  clientsClaim: true,
-  navigationPreload: true,
-  runtimeCaching: defaultCache,
+	precacheEntries: self.__SW_MANIFEST,
+	skipWaiting: true,
+	clientsClaim: true,
+	navigationPreload: true,
+	runtimeCaching: defaultCache,
+});
+
+self.addEventListener("push", (event) => {
+	if (event.data) {
+		const data = event.data.json();
+		const options = {
+			body: data.body,
+			icon: data.icon || "/icon-72x72.png",
+			badge: "/icon-48x48.png",
+			vibrate: [100, 50, 100],
+			data: {
+				dateOfArrival: Date.now(),
+				primaryKey: "2",
+			},
+		};
+		event.waitUntil(self.registration.showNotification(data.title, options));
+	}
+});
+
+self.addEventListener("notificationclick", (event) => {
+	console.log("Notification click received");
+	event.notification.close();
+	event.waitUntil(self.clients.openWindow("https://dolary.vercel.app"));
 });
 
 serwist.addEventListeners();

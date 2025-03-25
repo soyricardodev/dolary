51ca9139f25e7f8695aab4806c8d028ce3503fac
Ricardo Castro <soyricardodev@proton.me>
2025-03-24 23:39:27 -0400

    add db schema

---
+++ src/db/schema.ts
@@ -0,0 +1,62 @@
+import { sql } from "drizzle-orm";
+import {
+	integer,
+	real,
+	sqliteTableCreator,
+	text,
+} from "drizzle-orm/sqlite-core";
+
+export const createTable = sqliteTableCreator((name) => `dolary_${name}`);
+
+const id = integer("id").primaryKey({ autoIncrement: true });
+const createdAt = text("created_at")
+	.notNull()
+	.default(sql`(current_timestamp)`);
+const updatedAt = text("updated_at")
+	.notNull()
+	.default(sql`(current_timestamp)`)
+	.$onUpdate(() => new Date().toISOString());
+const isActive = integer("is_active", { mode: "boolean" })
+	.notNull()
+	.default(true);
+
+export const currencyTable = createTable("currency", {
+	id,
+	symbol: text("symbol").notNull(),
+	createdAt,
+	updatedAt,
+	isActive,
+});
+
+export const monitorTable = createTable("monitor", {
+	id,
+	idCurrency: integer("id_currency")
+		.references(() => currencyTable.id)
+		.notNull(),
+	key: text("key"),
+	title: text("title").notNull(),
+	price: real("price").notNull(),
+	priceOld: real("price_old"),
+	lastUpdate: integer("last_update", { mode: "timestamp" }).notNull(),
+	image: text("image"),
+	percent: real("percent").default(0.0),
+	change: real("change").default(0.0),
+	color: text("color").default("neutral"),
+	symbol: text("symbol").default(""),
+
+	createdAt,
+	updatedAt,
+	isActive,
+});
+
+export const historyTable = createTable("history", {
+	id,
+	idMonitor: integer("id_monitor")
+		.references(() => monitorTable.id)
+		.notNull(),
+	price: real("price").notNull(),
+	lastUpdate: integer("last_update", { mode: "timestamp" }).notNull(),
+	createdAt,
+	updatedAt,
+	isActive,
+});

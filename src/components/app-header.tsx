9396fcb869df95042574890af39f7e176573195d
Ricardo Castro <soyricardodev@proton.me>
2025-03-25 14:06:05 -0400

    add notifications manager to header

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -4,6 +4,7 @@ import { RefreshCw } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useCurrencyData } from "@/hooks/use-currency-data";
 import { Logo } from "./logo";
+import { PushNotificationManager } from "./notifications/push-notification-manager";
 
 export function AppHeader() {
 	const { refetch, isRefetching } = useCurrencyData();
@@ -12,7 +13,8 @@ export function AppHeader() {
 		<header className="flex w-full justify-between items-center">
 			<Logo />
 
-			<div className="flex justify-center">
+			<div className="flex justify-center gap-4">
+				<PushNotificationManager />
 				<Button size="icon" onClick={() => refetch()} disabled={isRefetching}>
 					<RefreshCw
 						className={`h-3.5 w-3.5 mr-1 ${isRefetching ? "animate-spin" : ""}`}

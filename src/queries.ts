bb027a74a218d2202a4c318671a29c05b7095d47
Ricardo Castro <soyricardodev@proton.me>
2025-03-25 19:37:57 -0400

    temp fix

--- src/queries.ts
+++ src/queries.ts
@@ -1,25 +1,44 @@
 import { unstable_cache } from "next/cache";
 import type { DolarApiResponse } from "./types";
 
-export const getDolarRates = unstable_cache(
-	async () => {
-		const response = await fetch(
-			"https://pydolarve.org/api/v1/dollar?rounded_price=false",
-			{
-				next: {
-					revalidate: 900,
-				},
+// export const getDolarRates = unstable_cache(
+// 	async () => {
+// 		const response = await fetch(
+// 			"https://pydolarve.org/api/v1/dollar?rounded_price=false",
+// 			{
+// 				next: {
+// 					revalidate: 900,
+// 				},
+// 			},
+// 		);
+
+// 		if (!response.ok) {
+// 			throw new Error("Network response was not ok");
+// 		}
+
+// 		const data = (await response.json()) as DolarApiResponse;
+
+// 		return data;
+// 	},
+// 	["rates"],
+// 	{ revalidate: 900, tags: ["rates"] },
+// );
+
+export const getDolarRates = async () => {
+	const response = await fetch(
+		"https://pydolarve.org/api/v1/dollar?rounded_price=false",
+		{
+			next: {
+				revalidate: 30,
 			},
-		);
+		},
+	);
 
-		if (!response.ok) {
-			throw new Error("Network response was not ok");
-		}
+	if (!response.ok) {
+		throw new Error("Network response was not ok");
+	}
 
-		const data = (await response.json()) as DolarApiResponse;
+	const data = (await response.json()) as DolarApiResponse;
 
-		return data;
-	},
-	["rates"],
-	{ revalidate: 900, tags: ["rates"] },
-);
+	return data;
+};

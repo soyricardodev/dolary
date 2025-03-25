348b9af8cebfdb409a1a6e7e734792f6ebe61e89
Ricardo Castro <soyricardodev@proton.me>
2025-03-25 15:09:42 -0400

    add custom promedio card props

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,41 +1,46 @@
 export interface DateTimeInfo {
-  date: string
-  time: string
+	date: string;
+	time: string;
 }
 
 export interface MonitorData {
-  change: number
-  color: string
-  image: string
-  last_update: string
-  percent: number
-  price: number
-  price_old: number
-  symbol: string
-  title: string
+	change: number;
+	color: string;
+	image: string;
+	last_update: string;
+	percent: number;
+	price: number;
+	price_old: number;
+	symbol: string;
+	title: string;
 }
 
 export interface Monitors {
-  bcv: MonitorData
-  enparalelovzla: MonitorData
-  [key: string]: MonitorData
+	bcv: MonitorData;
+	enparalelovzla: MonitorData;
+	[key: string]: MonitorData;
 }
 
 export interface DolarApiResponse {
-  datetime: DateTimeInfo
-  monitors: Monitors
+	datetime: DateTimeInfo;
+	monitors: Monitors;
 }
 
 export interface CurrencyCardProps {
-  title: string
-  price: number
-  symbol: string
-  change: number
-  percent: number
-  color: string
-  lastUpdate?: string
-  onClick: () => void
-  gradient: string
-  className?: string
+	title: string;
+	price: number;
+	symbol: string;
+	change: number;
+	percent: number;
+	color: string;
+	lastUpdate?: string;
+	onClick: () => void;
+	className?: string;
 }
 
+export interface CustomPromedio {
+	name: string;
+	price: number;
+}
+
+export const CustomPromedioKey = "customPromedio";

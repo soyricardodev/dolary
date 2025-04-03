170e0cecdae744e3e3fd9e7516adf571bd29b0d4
Ricardo Castro <soyricardodev@proton.me>
2025-04-03 19:25:45 -0400

    feat: integrate custom rate from local storage into Calculator component

--- src/components/calculator/responsive-calculator.tsx
+++ src/components/calculator/responsive-calculator.tsx
@@ -4,6 +4,8 @@ import { ResponsiveDialog } from "@/components/responsive-dialog";
 import { DialogTitle } from "@/components/ui/dialog";
 import { Calculator } from "./calculator";
 import { useCurrencyData } from "@/hooks/use-currency-data";
+import useLocalStorage from "@/hooks/use-local-storage";
+import { type CustomPromedio, CustomPromedioKey } from "@/types";
 
 interface ResponsiveCalculatorProps {
 	isOpen: boolean;
@@ -15,6 +17,9 @@ export function ResponsiveCalculator({
 	onCloseAction,
 }: ResponsiveCalculatorProps) {
 	const { data } = useCurrencyData();
+	const [customPromedio] = useLocalStorage<CustomPromedio>(CustomPromedioKey);
+
+	const customRate = customPromedio?.price ?? undefined;
 
 	if (!data) return;
 
@@ -26,7 +31,7 @@ export function ResponsiveCalculator({
 		<ResponsiveDialog open={isOpen} onOpenChange={onCloseAction}>
 			<DialogTitle className="my-4 text-center">Calculadora</DialogTitle>
 
-			<Calculator rates={{ bcv, paralelo, promedio }} />
+			<Calculator rates={{ bcv, paralelo, promedio, custom: customRate }} />
 		</ResponsiveDialog>
 	);
 }

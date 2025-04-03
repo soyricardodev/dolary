d3adafea4b0accbeb46e3cbaf2c0ec3a82f75842
Ricardo Castro <soyricardodev@proton.me>
2025-04-02 21:51:25 -0400

    feat: add compiler configuration to remove console logs in production

--- next.config.ts
+++ next.config.ts
@@ -1,45 +1,49 @@
 import withSerwistInit from "@serwist/next";
 
 const withSerwist = withSerwistInit({
-  swSrc: "./src/app/sw.ts",
-  swDest: "./public/sw.js",
+	swSrc: "./src/app/sw.ts",
+	swDest: "./public/sw.js",
 });
 
 export default withSerwist({
-  eslint: { ignoreDuringBuilds: true },
+	eslint: { ignoreDuringBuilds: true },
 	typescript: { ignoreBuildErrors: true },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          },
-        ],
-      },
-      {
-        source: '/sw.js',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'application/javascript; charset=utf-8',
-          },
-          {
-            key: 'Cache-Control',
-            value: 'no-cache, no-store, must-revalidate',
-          },
-        ],
-      },
-    ]
-  },
+	async headers() {
+		return [
+			{
+				source: "/(.*)",
+				headers: [
+					{
+						key: "X-Content-Type-Options",
+						value: "nosniff",
+					},
+					{
+						key: "X-Frame-Options",
+						value: "DENY",
+					},
+					{
+						key: "Referrer-Policy",
+						value: "strict-origin-when-cross-origin",
+					},
+				],
+			},
+			{
+				source: "/sw.js",
+				headers: [
+					{
+						key: "Content-Type",
+						value: "application/javascript; charset=utf-8",
+					},
+					{
+						key: "Cache-Control",
+						value: "no-cache, no-store, must-revalidate",
+					},
+				],
+			},
+		];
+	},
+	compiler: {
+		removeConsole:
+			process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false,
+	},
 });

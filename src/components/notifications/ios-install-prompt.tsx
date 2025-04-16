a923cfdd1dbd465db30cd96831f0f5344139c583
Ricardo Castro <soyricardodev@proton.me>
2025-04-16 14:57:35 -0400

    fix styles

--- src/components/notifications/ios-install-prompt.tsx
+++ src/components/notifications/ios-install-prompt.tsx
@@ -1,6 +1,14 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import {
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "../ui/dialog";
+import { Button } from "../ui/button";
 
 export function IOSInstallPrompt() {
 	const [isIOS, setIsIOS] = useState(false);
@@ -14,29 +22,35 @@ export function IOSInstallPrompt() {
 		setIsStandalone(window.matchMedia("(display-mode: standalone)").matches);
 	}, []);
 
-	if (isStandalone) {
-		return null; // Don't show install button if already installed
+	if (isStandalone || !isIOS) {
+		return null; // Don't show install button if already installed or not iOS
 	}
 
 	return (
-		<div>
-			<h3>Install App</h3>
-			<button type="button">Add to Home Screen</button>
-			{isIOS && (
+		<Dialog>
+			<DialogTrigger asChild>
+				<Button className="h-9 p-0 px-1.5 [&_svg]:size-5 hover:translate-x-[4px]! hover:translate-y-[4px]! hover:shadow-none bg-secondary-background text-foreground">
+					Instalar App
+				</Button>
+			</DialogTrigger>
+			<DialogContent className="max-w-sm">
+				<DialogHeader>
+					<DialogTitle>Instala la App</DialogTitle>
+				</DialogHeader>
 				<p>
-					To install this app on your iOS device, tap the share button
+					Para instalar la App en tu dispositivo iOS, toca el botón de compartir
 					<span role="img" aria-label="share icon">
 						{" "}
 						⎋{" "}
 					</span>
-					and then "Add to Home Screen"
+					y luego toca "Agregar a la pantalla de inicio"
 					<span role="img" aria-label="plus icon">
 						{" "}
 						➕{" "}
 					</span>
 					.
 				</p>
-			)}
-		</div>
+			</DialogContent>
+		</Dialog>
 	);
 }

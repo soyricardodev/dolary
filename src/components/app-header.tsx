f88d24fd505de1e2adee25e5d0e3ded0f80ce39b
Ricardo Castro <soyricardodev@proton.me>
2025-04-16 14:57:27 -0400

    Add theme switcher and ios install prompt

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -8,6 +8,8 @@ import { PushNotificationManager } from "./notifications/push-notification-manag
 import { revalidateRates } from "./actions";
 import PwaInstallPrompt from "./pwa-install-prompt";
 import { ShareOnWhatsapp } from "./share-on-whatsapp";
+import { ThemeSwitcher } from "./theme-switcher";
+import { IOSInstallPrompt } from "./notifications/ios-install-prompt";
 
 export function AppHeader() {
 	const { refetch, isRefetching } = useCurrencyData();
@@ -23,16 +25,19 @@ export function AppHeader() {
 
 			<div className="flex justify-center gap-4">
 				<PwaInstallPrompt />
+				<IOSInstallPrompt />
 				<PushNotificationManager />
+				<ThemeSwitcher />
 				<ShareOnWhatsapp text="Mira las tasas del Dólar" />
 				<Button
 					size="icon"
 					onClick={() => getFreshData()}
 					disabled={isRefetching}
 					aria-label="Refrescar Datos"
+					className="size-9 p-0 [&_svg]:size-5 hover:translate-x-[4px]! hover:translate-y-[4px]! hover:shadow-none bg-secondary-background "
 				>
 					<RefreshCwIcon
-						className={`h-3.5 w-3.5 mr-1 ${isRefetching ? "animate-spin" : ""}`}
+						className={`h-3.5 w-3.5 mr-1 stroke-foreground ${isRefetching ? "animate-spin" : ""}`}
 					/>
 				</Button>
 			</div>

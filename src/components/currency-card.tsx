7b7143fcc37c4f5c9aebf239e4094b92a7c94731
Ricardo Castro <soyricardodev@proton.me>
2025-04-21 19:23:37 -0400

    Refactor CurrencyCard layout for improved spacing and structure; adjust Badge and Button styles

--- src/components/currency-card.tsx
+++ src/components/currency-card.tsx
@@ -52,31 +52,29 @@ export function CurrencyCard({
 
 	return (
 		<Card className={cn("relative", className)} onClick={onClick}>
-			<CardHeader className="pt-2 pb-0 flex flex-row w-full justify-between items-center">
-				<div className="flex gap-4">
-					<div className="flex flex-col">
-						<h2 className="text-xl font-bold">{title}</h2>
-					</div>
-					{
-						<Badge className="w-max" variant={"neutral"}>
-							{isChangePositive ? (
-								<ArrowUpIcon className="size-3 mr-1" />
-							) : (
-								<ArrowDownIcon className="size-3 mr-1" />
-							)}
+			<CardHeader className="pt-2 pb-0 flex flex-row w-full justify-between space-y-0">
+				<div className="flex gap-4 space-y-0">
+					<h2 className="text-xl font-bold">{title}</h2>
+					<Badge className="w-fit h-fit px-1" variant={"neutral"}>
+						{isChangePositive ? (
+							<ArrowUpIcon className="size-3 mr-1" />
+						) : (
+							<ArrowDownIcon className="size-3 mr-1" />
+						)}
 
-							<span className="font-medium">
-								{isChangePositive ? "+" : "-"}
-								{formatCurrency(change ?? 0)} ({(percent ?? 0).toFixed(2)}%)
-							</span>
-						</Badge>
-					}
+						<span className="font-medium">
+							{isChangePositive ? "+" : "-"}
+							{formatCurrency(change ?? 0)} ({(percent ?? 0).toFixed(2)}%)
+						</span>
+					</Badge>
 				</div>
 				<Button
 					onClick={handleCopy}
 					title="Copiar valor"
 					size="icon"
 					aria-label="Copiar Tasa"
+					className="size-8 p-0 [&_svg]:size-4 hover:translate-x-[4px]! hover:translate-y-[4px]! hover:shadow-none"
+					type="button"
 				>
 					{copied ? (
 						<CheckIcon className="h-4 w-4" />
@@ -86,27 +84,30 @@ export function CurrencyCard({
 				</Button>
 			</CardHeader>
 			<CardContent className="py-0">
-				<div className="relative h-full flex flex-col">
+				<div className="h-full flex flex-col">
 					<div className="flex items-baseline mb-1">
 						<span className="text-3xl font-bold">{formatCurrency(price)}</span>
 						{symbol != null && <span className="ml-1 text-xs">{symbol}</span>}
 					</div>
 				</div>
-			</CardContent>
-			<CardFooter className="flex items-center gap-1 justify-between pb-3">
-				<div className="mt-auto text-xs">
-					{lastUpdate != null ? lastUpdate : null}
-					{subtitle && (
-						<p className="flex items-center gap-1 text-xs text-muted-foreground mt-0.5">
-							{subtitle}
-						</p>
-					)}
-				</div>
 
-				<Button size="sm" variant="neutral">
-					Calcular
-				</Button>
-			</CardFooter>
+				<div className="flex justify-between items-center w-full">
+					<div className="mt-auto text-xs">
+						{lastUpdate != null ? lastUpdate : null}
+						{subtitle && (
+							<p className="flex items-center gap-1 text-xs text-muted-foreground mt-0.5">
+								{subtitle}
+							</p>
+						)}
+					</div>
+
+					<Button size="sm" variant="neutral">
+						Calcular
+					</Button>
+				</div>
+			</CardContent>
+			{/*<CardFooter className="flex items-center gap-1 justify-between pb-3">
+			</CardFooter>*/}
 		</Card>
 	);
 }

27bf1b02c1df4bb6cac7b3eb50fb7ead5772243c
Ricardo Castro <soyricardodev@proton.me>
2025-04-22 00:18:37 -0400

    Refactor manifest function for improved readability and consistency in formatting

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,25 +1,26 @@
-import type { MetadataRoute } from 'next'
- 
+import type { MetadataRoute } from "next";
+
 export default function manifest(): MetadataRoute.Manifest {
-  return {
-    name: 'Calculadora del Dolar en Venezuela | Dolary',
-    short_name: 'Dolary',
-    description: 'Una calculadora sin anuncios, rapida y sin conexion a internet para calcular el precio del dolar en Venezuela',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#ffffff',
-    theme_color: '#000000',
-    icons: [
-      {
-        src: '/icon-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: '/icon-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
-    ],
-  }
+	return {
+		name: "Calculadora del Dolar en Venezuela | Dolary",
+		short_name: "Dolary",
+		description:
+			"Una calculadora sin anuncios, rapida y sin conexion a internet para calcular el precio del dolar en Venezuela",
+		start_url: "/",
+		display: "standalone",
+		background_color: "#ffffff",
+		theme_color: "#000000",
+		icons: [
+			{
+				src: "/icon-192x192.png",
+				sizes: "192x192",
+				type: "image/png",
+			},
+			{
+				src: "/icon-512x512.png",
+				sizes: "512x512",
+				type: "image/png",
+			},
+		],
+	};
 }
